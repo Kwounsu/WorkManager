9dcdf4b471c98f7a8a4ce3de4f703c35afedb123
Kwounsu <sb00lgs@gmail.com>
2020-09-25 14:41:30 -0700

    data binding for the button

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -2,24 +2,26 @@ package com.example.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.work.*
-import java.util.concurrent.TimeUnit
+import com.example.workmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            lifecycleOwner = this@MainActivity
+            activity = this@MainActivity
+        }
     }
 
-    private fun doWorkOneTime() {
+    fun doWorkOneTime() {
         val workRequest = OneTimeWorkRequestBuilder<MyWorker>().build()
-        val workManager = WorkManager.getInstance()
-        workManager.enqueue(workRequest)
-    }
-
-    private fun doWorkPeriodic() {
-        val workRequest = PeriodicWorkRequestBuilder<MyWorker>(15, TimeUnit.MINUTES).build()
-        PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES).build()
         val workManager = WorkManager.getInstance()
         workManager.enqueue(workRequest)
     }
